38e6339167dbcfb52fa9f5fabcdf916236c47c08
Petar Ivanov <pepincho95@gmail.com>
2013-07-09 18:38:33 +0300

    promqna na vruzki

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,5 +2,5 @@ class Role < ActiveRecord::Base
   attr_accessible :accountast, :searcher, :softwareEngineer, :teacher
 
   has_many :role_activities
-  has_many :users
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :city_id, :email, :name, :role_id
 
-  belongs_to :role
+  has_one :role
   belongs_to :city
 end
