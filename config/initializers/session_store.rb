a09d504147bc21668d5d61985fd58104043e9051
Petar Ivanov <pepincho95@gmail.com>
2013-07-09 17:02:12 +0300

    kris

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TheBusyNet::Application.config.session_store :cookie_store, key: '_TheBusyNet_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TheBusyNet::Application.config.session_store :active_record_store
