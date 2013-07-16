2535828fc7e1b63205cf5f1e6938a6dc9cb1ad95
Petar Ivanov <pepincho95@gmail.com>
2013-07-16 22:39:12 +0300

    logo, style i sign out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,17 @@
 class ApplicationController < ActionController::Base
 	
-  protect_from_forgery
-  def after_sign_in_path_for(resource_or_scope)
-  	if resource_or_scope.is_a?(AdminUser) 
-		admin_dashboard_path
-	else
-  		currnet_user_properties_path
+  	protect_from_forgery
+  	def after_sign_in_path_for(resource_or_scope)
+  		if resource_or_scope.is_a?(AdminUser) 
+			admin_dashboard_path
+		else
+  			currnet_user_properties_path
+  		end
   	end
-  end
+	
+	def after_sign_out_path_for(resource)
+		home_path = "/users/sign_in"
+ 		respond_to?(home_path, true) ? send(root_path) : home_path
+	end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -65,6 +65,13 @@ ActiveRecord::Schema.define(:version => 20130711055203) do
     t.datetime "updated_at",  :null => false
   end
 
+  create_table "role_categories", :force => true do |t|
+    t.integer  "role_id"
+    t.integer  "category_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
   create_table "roles", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
