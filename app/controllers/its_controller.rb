53e97b9affe778ac897175ff606a40f15da0b711
ivan <kr1sk0@abv.bg>
2013-08-06 09:52:33 +0300

    opit za 4astit4no podobrqvane na search-a za specialisti

--- app/controllers/its_controller.rb
+++ app/controllers/its_controller.rb
@@ -4,9 +4,9 @@ class ItsController < ApplicationController
     @its = User.where(role_id: Role.find_by_name('IT').id)
     @cities = City.all
     @activities = Activity.all
+   
+    @its = @its.joins(:ad_activities).where(activity_id: params[:activity_id]) if params[:activity_id].present?
+    @its = @its.where(city_id: params[:city_id]) if params[:city_id].present?
     
-    if params[:city_id].present?
-      @its = @its.where(city_id: params[:city_id])
-    end
   end
 end
