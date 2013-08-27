f0624d0e8526d97009dd4ea55a16e50a7cd20e32
ivan <kr1sk0@abv.bg>
2013-08-27 12:18:09 +0300

    showing activities by role

--- app/controllers/its_controller.rb
+++ app/controllers/its_controller.rb
@@ -1,8 +1,9 @@
 class ItsController < ApplicationController
   def index
-    @its = User.where(role_id: Role.find_by_name(params[:role]).id)
+    section = Role.find_by_name(params[:role])
+    @its = User.where(role_id: section.id)
     @cities = City.all
-    @activities = Activity.all
+    @activities = section.activities
    
     @its = @its.joins(:user_activities).where('user_activities.activity_id in (' + params[:activity_id].join(',') + ')') if params[:activity_id].present?
     @its = @its.where(city_id: params[:city_id]) if params[:city_id].present?
