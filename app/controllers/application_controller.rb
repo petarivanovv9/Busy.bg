05745a5bb57e50fd474d4218ac9d5fd4f24941ed
ivan <kr1sk0@abv.bg>
2013-08-28 11:58:06 +0300

    redesign sign in and sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,19 @@
 class ApplicationController < ActionController::Base
 	
   	protect_from_forgery
-  	def after_sign_in_path_for(resource_or_scope)
-  		if resource_or_scope.is_a?(AdminUser) 
-			 admin_dashboard_path
-		  else
-  		  user_profile_path(user_id: current_user.id)
-  		end
+
+  def after_sign_in_path_for(resource_or_scope)
+  	if resource_or_scope.is_a?(AdminUser) 
+			admin_dashboard_path
+		else
+  		user_profile_path(user_id: current_user.id)
   	end
+  end
 	
 	def after_sign_out_path_for(resource)
 		home_path = "/users/sign_in"
  		respond_to?(home_path, true) ? send(root_path) : home_path
 	end
 
+  
 end
