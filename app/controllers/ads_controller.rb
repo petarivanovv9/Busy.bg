14652206d98d3b9c250182ffe994fd0767a5ba53
cerebraldeath <hristodd@gmail.com>
2013-09-08 19:46:19 +0300

    ad creation fix

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -3,16 +3,16 @@ class AdsController < InheritedResources::Base
     @ads = Ad.scoped
     @cities = City.all
     @activities = Activity.all
-    
+
     @ads = @ads.where(city_id: params[:city_id]) if params[:city_id].present?
     @ads = @ads.joins(:ad_activities).where('ad_activities.activity_id = '['?'], params[:activity_id]) if params[:activity_id].present?
     @ads = @ads.where(max_price: params[:max_price]) if params[:max_price].present?
   end
 
- 
+
   def create
     ad_params = params[:ad]
-    
+
     ad = Ad.new
     ad.user_id = current_user.id
     ad.max_price = ad_params[:max_price]
@@ -22,12 +22,21 @@ class AdsController < InheritedResources::Base
     # ad.deadline = Date.new ad_params["deadline(1i)"].to_i, ad_params["deadline(2i)"].to_i, ad_params["deadline(3i)"].to_i
     ad.save
 
-    ad_params[:ad_activities].each do |i|
-      if i.present?
-        ad_activity = AdActivity.new
-        ad_activity.ad_id = ad.id
-        ad_activity.activity_id = i
-        ad_activity.save
+    if ad_params[:ad_activities].is_a? Array
+      activities = ad_params[:ad_activities].join(',')
+    else
+      activities = ad_params[:ad_activities]
+    end
+
+    if activities.present?
+      puts "here"
+      activities.split(',').each do |i|
+        if i.present?
+          ad_activity = AdActivity.new
+          ad_activity.ad_id = ad.id
+          ad_activity.activity_id = i
+          ad_activity.save
+        end
       end
     end
 
@@ -37,15 +46,15 @@ class AdsController < InheritedResources::Base
   def edit
     @ad = Ad.find(params[:id])
 
-    if current_user.id != @ad.user_id 
+    if current_user.id != @ad.user_id
      redirect_to ads_path
-    end 
+    end
   end
 
   def destroy
     @ad = Ad.find(params[:id])
 
-    if current_user.id == @ad.user_id 
+    if current_user.id == @ad.user_id
       @ad.destroy
     end
 
@@ -61,4 +70,4 @@ class AdsController < InheritedResources::Base
    render nothing: true
   end
 end
- 
+
